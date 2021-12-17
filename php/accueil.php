485e2254d53a0fd35e83291eb0bb1bc931f585f8
Lorvaill <96297088+Lorvaill@users.noreply.github.com>
2021-12-17 14:42:04 +0100

    projet3

--- php/accueil.php
+++ php/accueil.php
@@ -1,7 +1,7 @@
 <?php
 echo '<div class="sec" id="sec">';
 echo '<h2>Bienvenue sur mon site web !</h2>';
-echo "<p> Je m'apelle Théo Ledain et je suis actuellement en classe de première année de BTS SIO<br><br>Je souhaite particulièrement apprendre le language C et C++</p>";
+echo "<p> Je m'apelle Théo Ledain et je suis actuelllllllllllement en classe de première année de BTS SIO<br><br>Je souhaite particulièrement apprendre le language C et C++</p>";
 echo '<br><img class="moi" src="../images/moi.jpeg">';
 echo '</div>';
 ?>
